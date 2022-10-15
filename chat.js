d55e9c5b39f60d267dfb6474c3e90fc2cdf05b0e
sandhyadav <66108338+sandhyadav@users.noreply.github.com>
2022-10-15 15:29:03 +0530

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+//ADD YOUR FIREBASE LINKS HERE
+user_name = localStorage.getItem("user_name");
+room_name = localStorage.getItem("room_name");
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+
+
+
+
